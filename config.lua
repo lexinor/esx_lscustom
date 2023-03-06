37e7b4c3413061c8fc49eed11a719ea02ab11a79
Alessandro Alterno <28725795+lexinor@users.noreply.github.com>
2023-03-06 09:52:38 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config                   = {}
 Config.DrawDistance      = 10.0
 Config.Locale = GetConvar('esx:locale', 'en')
-Config.IsMechanicJobOnly = false
+Config.IsMechanicJobOnly = true
 
 Config.Zones = {
 
